4ee0222297e117b37b5760e29f72c18229cdcb4b
Kostya Vakarian <vakarianplay@gmail.com>
2025-11-28 13:05:15 +0300

    Update telegrambot.go

--- offer_bot/telegrambot.go
+++ offer_bot/telegrambot.go
@@ -211,7 +211,7 @@ func TelegramBot(botApi, content_, users_, startM_, sendM_ string) {
 		userName := m.Sender.FirstName + " " + m.Sender.LastName
 		// bot.Send(m.Sender, "*Привет, "+userName+"*\n\n_Этот бот - предложка._\nПросто скинь сюда мем, который ты хочешь запостить.", markdown)
 		bot.Send(m.Sender, userName+"\n"+startM_, markdown)
-		bot.Send(m.Sender, "🌐Бесплатный и быстрый VPN: *https://raspad.space/vpn https://t.me/raspad_vpn*", markdown)
+		bot.Send(m.Sender, "📊 Разработчик бота: *https://t.me/cyberbibki \n https://vakarian.website*", markdown)
 		// bot.Send(m.Sender, "↓ выбери дальнейшее действие ↓", menu)
 
 	})
